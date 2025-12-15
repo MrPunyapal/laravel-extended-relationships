fd8214934978c9e3f716b3976da85c3d4910e08c
Punyapal Shah <mrpunyapal@gmail.com>
2025-12-15 12:26:18 +0530

    feat: add rector configuration for Pest code quality

---
+++ rector.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorPest\Set\PestSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSets([
+         PestSetList::PEST_CODE_QUALITY,
+    ]);
