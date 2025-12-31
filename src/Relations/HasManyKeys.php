8d07c415d284c08da6ad56d7d10b7bdc4ba30b3a
Punyapal Shah <mrpunyapal@gmail.com>
2025-12-31 21:01:54 +0530

    fix: enhance documentation for HasManyKeys with generics type hints

--- src/Relations/HasManyKeys.php
+++ src/Relations/HasManyKeys.php
@@ -9,6 +9,12 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 
+/**
+ * @template TRelatedModel of \Illuminate\Database\Eloquent\Model
+ * @template TDeclaringModel of \Illuminate\Database\Eloquent\Model
+ *
+ * @extends \Illuminate\Database\Eloquent\Relations\Relation<TRelatedModel, TDeclaringModel, object>
+ */
 class HasManyKeys extends Relation
 {
     /**
@@ -27,6 +33,11 @@ class HasManyKeys extends Relation
 
     /**
      * Create a new has one or many relationship instance.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder<TRelatedModel>  $query
+     * @param  TDeclaringModel  $parent
+     * @param  array<string, string>  $relations
+     * @param  string  $localKey
      */
     public function __construct(Builder $query, Model $parent, array $relations, protected string $localKey)
     {
